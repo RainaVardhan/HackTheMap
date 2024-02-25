a6376cd88db9b01a7333df1ce1b36963ecb2e3d4
anutishah <jyr3um@virginia.edu>
2024-02-25 02:57:34 -0500

    continuing to debug p5

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
 # from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import RedirectView
+from django.urls import path, include
+# from django.views.generic import RedirectView
 
 urlpatterns = [
-    re_path(r'^$', RedirectView.as_view(url='/VoyageVault/login/', permanent=True)),
+    #re_path(r'^$', RedirectView.as_view(url='/VoyageVault/login/', permanent=True)),
     path("VoyageVault/", include("VoyageVault.urls")),
     # path("admin/", admin.site.urls),
     # path("", include("VoyageVault.urls"))
