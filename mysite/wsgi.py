c24186b13c0f8ffc704bdd51167bd8c1620c9072
anutishah <jyr3um@virginia.edu>
2024-02-25 00:56:38 -0500

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite1.mysite1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
