a9902679fa6e4a8a6af96b93f118e89da6e38ca5
RainaVardhan <cdw3pe@virginia.edu>
2024-02-25 05:15:15 -0500

    added bootstrap

--- mysite/settings.py
+++ mysite/settings.py
@@ -161,28 +161,30 @@ USE_I18N = True
 
 USE_TZ = True
 
+import django_heroku
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
 LOGIN_URL = '/VoyageVault/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = '/media/'   ##maybe might only need the slash at the end ???? idk tho
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 WHITENOISE_KEEP_ONLY_HASHED_FILES = True
-
 
 
 
