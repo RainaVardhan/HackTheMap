a73259ce3b65483b61f90589255e7c557c63078c
anutishah <jyr3um@virginia.edu>
2024-02-25 02:19:51 -0500

    data

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,23 +90,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-if IS_HEROKU_APP:
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age = 600,
-            conn_health_checks = True,
-            ssl_require = True,
-        ),
+DATABASES = {
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
-
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if IS_HEROKU_APP:
+#     DATABASES = {
+#         'default': dj_database_url.config(
+#             conn_max_age = 600,
+#             conn_health_checks = True,
+#             ssl_require = True,
+#         ),
+#
+#
+#         }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
