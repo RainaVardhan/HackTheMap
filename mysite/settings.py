65ddd3313d571713e82be1a7548c2c511ef22560
anutishah <jyr3um@virginia.edu>
2024-02-25 01:58:33 -0500

    bootsbootsboots

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.bootstrap5'
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
