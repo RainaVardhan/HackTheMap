feb2636c7e9771840b990248ddc2739904949cad
RainaVardhan <cdw3pe@virginia.edu>
2024-02-27 14:16:18 -0500

    fixed background formatting

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-s#nh#k0j0+h8w9v93gvogxau6l2b$)gx5l1-2e&66jm$hrtt3k'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-s#nh#k0j0+h8w9v93gvogxau6l2b$)gx5l1-2e&66jm$hrtt3k'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
